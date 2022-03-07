6ca69f7edf1d58ff78e52dd9fca7c3a0a79227d0
Kevin04C <kevincespedes2004@gmail.com>
2022-03-07 15:14:54 -0500

    new improvements

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,9 +1,14 @@
 export function PostCard(props) {
-  let { date, title, slug, _embedded } = props;
+  let { date, title, slug, _embedded, id } = props;
   let dateFormat = new Date(date).toLocaleString();
   let urlPoster = _embedded["wp:featuredmedia"][0].source_url
     ? _embedded["wp:featuredmedia"][0].source_url
     : "app/assets/favicon.png";
+
+  document.addEventListener("click", (e) => {
+    if (!e.target.matches(".post-card a")) false;
+    localStorage.setItem("id", `${e.target.dataset.id}`);
+  });
 
   return `
     <article class="post-card">
@@ -11,7 +16,7 @@ export function PostCard(props) {
     <h2>${title.rendered}</h2>
     <p>
     <time datetime="${date}">${dateFormat.replace(",", " - ")}</time>
-    <a href="#${slug}">Ver</a>
+    <a href="#${slug}" data-id"${id}">Ver Publicación</a>
     </p>
     </article>
   `;
--- app/components/Title.js
+++ app/components/Title.js
@@ -3,7 +3,9 @@ export function title() {
   const h1 = document.createElement("h1");
 
   h1.innerHTML = `
-  <a href="${api.DOMINAN}" target="_blank">${api.NAME.toUpperCase()}</a>
+  <a href="${
+    api.DOMINAN
+  }" target="_blank" data-kevin="kevin">${api.NAME.toUpperCase()}</a>
   `;
   return h1;
 }
